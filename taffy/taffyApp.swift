cd8531ef8ee6345403337389a431b2b2e94eb414
walnut-pro <takumu.yoshiba@rakuten.com>
2022-11-29 21:22:24 +0900

    Initial Commit

---
+++ taffy/taffyApp.swift
@@ -0,0 +1,17 @@
+//
+//  taffyApp.swift
+//  taffy
+//
+//  Created by Yoshiba, Takumu | Takumu | CTMO on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct taffyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
